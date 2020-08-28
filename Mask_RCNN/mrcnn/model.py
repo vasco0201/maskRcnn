d46a502a3e284da7aed0e9400db9715f1b0515c7
Vasco <vasco_leal_500@hotmail.com>
2020-08-28 22:19:33 +0100

    changed modelcheckpoint to save the best only

--- Mask_RCNN/mrcnn/model.py
+++ Mask_RCNN/mrcnn/model.py
@@ -2353,7 +2353,7 @@ class MaskRCNN():
             keras.callbacks.TensorBoard(log_dir=self.log_dir,
                                         histogram_freq=0, write_graph=True, write_images=False),
             keras.callbacks.ModelCheckpoint(self.checkpoint_path,
-                                            verbose=0, save_weights_only=True),
+                                            verbose=0, save_best_only=True, monitor='val_loss', mode='min')
         ]
 
         # Add custom callbacks to the list
